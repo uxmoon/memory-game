0c3f77b45790a5da49c604cb709e637f8a04a1c6
uxmoon <uxmoon@gmail.com>
2023-09-11 16:24:04 -0300

    Data fetching and set state

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,35 @@
+import { useEffect, useState } from 'react'
+
 export default function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  const [cards, setCards] = useState([])
+  // get images
+  useEffect(() => {
+    fetch(
+      'https://fed-team.modyo.cloud/api/content/spaces/animals/types/game/entries?per_page=20'
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setCards(data.entries)
+      })
+  }, [])
+
+  console.log(cards)
+
+  return (
+    <div>
+      <h1>Memory Game</h1>
+      <ul>
+        {cards.slice(0, 9).map((card) => (
+          <li key={card.fields.image.uuid}>
+            <img
+              src={card.fields.image.url}
+              alt={card.fields.image.title}
+              width='200'
+            />
+            {card.fields.image.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
